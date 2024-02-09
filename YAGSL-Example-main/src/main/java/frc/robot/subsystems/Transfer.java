468abc80bf70dd1a63134ddc848c126b34b625b7
lregueiferos <124931111+lregueiferos@users.noreply.github.com>
2024-02-09 11:35:25 -0500

    transfer system update

--- YAGSL-Example-main/src/main/java/frc/robot/subsystems/Transfer.java
+++ YAGSL-Example-main/src/main/java/frc/robot/subsystems/Transfer.java
@@ -1,9 +1,12 @@
 package frc.robot.subsystems;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+
 public class Transfer {
 private TalonFX TransferMotor1;
 private TalonFX TransferMotor2;
+ DigitalInput transferstop = new DigitalInput(1);
 
     
 public Transfer(TalonFX TransferMotor1, TalonFX TransferMotor2)
@@ -12,9 +15,10 @@ public Transfer(TalonFX TransferMotor1, TalonFX TransferMotor2)
     this.TransferMotor2 = TransferMotor2;
     
   }
-  public void moveLauncher(double TransferSpeed1, double TransferSpeed2){
+  public void activatetransfer(double TransferSpeed1, double TransferSpeed2){
     TransferMotor1.set(TransferSpeed1);
     TransferMotor2.set(TransferSpeed2);
+    
   }
 }
 
